b12fd04ed20c0e6b804f5c630101b03e4c40df65
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-27 14:29:47 +0330

    Initial commit

---
+++ src/main/java/de/miladsa/behpaya/BehpayaApplication.java
@@ -0,0 +1,13 @@
+package de.miladsa.behpaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehpayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BehpayaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/miladsa/behpaya/BehpayaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.miladsa.behpaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehpayaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
