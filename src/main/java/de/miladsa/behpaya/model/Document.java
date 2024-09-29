c6891c1550ea0085e669322d7b80a1148cabb12f
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-29 14:57:07 +0330

    Implementing Document entity class

---
+++ src/main/java/de/miladsa/behpaya/model/Document.java
@@ -0,0 +1,35 @@
+package de.miladsa.behpaya.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "documents")
+public class Document extends Metadata {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "varchar(100)", nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "varchar(255)")
+    private String description;
+
+    @OneToMany(mappedBy = "document")
+    private List<Board> boards;
+
+    @OneToMany(mappedBy = "document")
+    private List<Calculation> calculations;
+}
