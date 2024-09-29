48d201772439b73f01a8294c5af8b2418d3628ea
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-29 14:59:30 +0330

    Implementing HowToCalculate entity class

---
+++ src/main/java/de/miladsa/behpaya/model/HowToCalculate.java
@@ -0,0 +1,24 @@
+package de.miladsa.behpaya.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "HowToCalculate")
+public class HowToCalculate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "how_to_calculate_description", columnDefinition = "varchar(500)", nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "indicator_id")
+    private Indicator indicator;
+}
