b61cf715914a69238d3463a44c8ba62504902181
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-29 14:57:48 +0330

    Implementing Board entity class

---
+++ src/main/java/de/miladsa/behpaya/model/Board.java
@@ -0,0 +1,39 @@
+package de.miladsa.behpaya.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "boards")
+public class Board extends Metadata {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "varchar(100)", nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "varchar(500)")
+    private String description;
+
+    @Column(unique = true, nullable = false)
+    private Integer startNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "document_id")
+    private Document document;
+
+    @OneToMany(mappedBy = "board")
+    private List<Indicator> indicators;
+}
