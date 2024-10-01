580ebd76e1502449a332d57db970a478d9412979
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-10-01 21:50:53 +0330

    Creating a bean for custom aware of user

--- src/main/java/de/miladsa/behpaya/BehpayaApplication.java
+++ src/main/java/de/miladsa/behpaya/BehpayaApplication.java
@@ -1,13 +1,24 @@
 package de.miladsa.behpaya;
 
+import de.miladsa.behpaya.configuration.CustomAuditorAware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.userdetails.User;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BehpayaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BehpayaApplication.class, args);
+    }
+
+    @Bean
+    public AuditorAware<User> auditorAware() {
+        return new CustomAuditorAware();
     }
 
 }
