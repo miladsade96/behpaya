874c41bb92c1f438676e7b85a2fce2b29d0fc611
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-10-05 20:44:22 +0330

    Using one classes

--- src/main/java/de/miladsa/behpaya/model/Translation.java
+++ src/main/java/de/miladsa/behpaya/model/Translation.java
@@ -3,18 +3,21 @@ package de.miladsa.behpaya.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-
-@EqualsAndHashCode(callSuper = true)
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "translations")
-public class Translation extends AvailableTranslations {
+public class Translation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(columnDefinition = "varchar(1000)", nullable = false)
+    private String fa;
+
+    @Column(columnDefinition = "varchar(1000)", nullable = false)
+    private String en;
 }
