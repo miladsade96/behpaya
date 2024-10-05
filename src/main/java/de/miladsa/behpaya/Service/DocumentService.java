49551209de732ae3f491bfd8fc942c7deceddfd7
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-10-05 21:03:36 +0330

    Saving entire document at once

--- src/main/java/de/miladsa/behpaya/Service/DocumentService.java
+++ src/main/java/de/miladsa/behpaya/Service/DocumentService.java
@@ -1,9 +1,11 @@
 package de.miladsa.behpaya.Service;
 
-import de.miladsa.behpaya.dtos.DocumentProjection;
-import de.miladsa.behpaya.model.Document;
-import de.miladsa.behpaya.repository.DocumentRepository;
-import de.miladsa.behpaya.validators.DocumentValidator;
+import de.miladsa.behpaya.dto.*;
+import de.miladsa.behpaya.model.*;
+import de.miladsa.behpaya.projection.DocumentProjection;
+import de.miladsa.behpaya.repository.*;
+import de.miladsa.behpaya.validators.*;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +14,24 @@ import java.util.List;
 import java.util.Set;
 
 @Service
+@Transactional
 public class DocumentService {
     private DocumentRepository documentRepository;
     private DocumentValidator documentValidator;
+    private BoardRepository boardRepository;
+    private IndicatorRepository indicatorRepository;
+    private HowToCalculateRepository howToCalculateRepository;
+    private CalculationRepository calculationRepository;
+    private BoardValidator boardValidator;
+    private TranslationValidator translationValidator;
+    private IndicatorValidator indicatorValidator;
+    private HowToCalculateValidator howToCalculateValidator;
+    private CalculationValidator calculationValidator;
+
+    @Autowired
+    public void setDocumentRepository(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
 
     @Autowired
     public void setDocumentValidator(DocumentValidator documentValidator) {
@@ -22,20 +39,116 @@ public class DocumentService {
     }
 
     @Autowired
-    public void setDocumentRepository(DocumentRepository documentRepository) {
-        this.documentRepository = documentRepository;
+    public void setBoardRepository(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @Autowired
+    public void setIndicatorRepository(IndicatorRepository indicatorRepository) {
+        this.indicatorRepository = indicatorRepository;
+    }
+
+    @Autowired
+    public void setHowToCalculateRepository(HowToCalculateRepository howToCalculateRepository) {
+        this.howToCalculateRepository = howToCalculateRepository;
+    }
+
+    @Autowired
+    public void setCalculationRepository(CalculationRepository calculationRepository) {
+        this.calculationRepository = calculationRepository;
+    }
+
+    @Autowired
+    public void setBoardValidator(BoardValidator boardValidator) {
+        this.boardValidator = boardValidator;
+    }
+
+    @Autowired
+    public void setTranslationValidator(TranslationValidator translationValidator) {
+        this.translationValidator = translationValidator;
+    }
+
+    @Autowired
+    public void setIndicatorValidator(IndicatorValidator indicatorValidator) {
+        this.indicatorValidator = indicatorValidator;
+    }
+
+    @Autowired
+    public void setHowToCalculateValidator(HowToCalculateValidator howToCalculateValidator) {
+        this.howToCalculateValidator = howToCalculateValidator;
+    }
+
+    @Autowired
+    public void setCalculationValidator(CalculationValidator calculationValidator) {
+        this.calculationValidator = calculationValidator;
     }
 
     public List<DocumentProjection> getAllDocuments() {
         return documentRepository.findAllDocuments();
     }
 
-    public Set<String> addADocument(Document document) {
+    public Set<String> addADocument(DocumentDTO documentDTO) {
+        var document = new Document();
+        document.setTitle(documentDTO.getTitle());
+        document.setDescription(documentDTO.getDescription());
         var violations = documentValidator.validator(document);
+        var savedDoc = documentRepository.save(document);
+
+        var boards = documentDTO.getBoards();
+        for (BoardDTO board_dto : boards) {
+            var board = new Board();
+            board.setTitle(board_dto.getTitle());
+            board.setDescription(board_dto.getDescription());
+            board.setDocument(savedDoc);
+            board.setStartNumber(board_dto.getStartNumber());
+            violations.addAll(boardValidator.validator(board));
+            var savedBoard = boardRepository.save(board);
+
+            for (IndicatorDTO indicator_dto : board_dto.getIndicators()) {
+                var indicator = new Indicator();
+                indicator.setName(indicator_dto.getName());
+                indicator.setType(indicator_dto.getType());
+                indicator.setIndicatorType(indicator_dto.getIndicatorType());
+                indicator.setUnit(indicator_dto.getUnit());
+                indicator.setBoard(savedBoard);
+
+                var translation = new Translation();
+                translation.setEn(indicator_dto.getTranslationDTO().getEn());
+                translation.setFa(indicator_dto.getTranslationDTO().getFa());
+                violations.addAll(translationValidator.validator(translation));
+                indicator.setTranslation(translation);
+
+                var translationDescription = new Translation();
+                translationDescription.setEn(indicator_dto.getTranslationDescriptionDTO().getEn());
+                translationDescription.setFa(indicator_dto.getTranslationDescriptionDTO().getFa());
+                violations.addAll(translationValidator.validator(translationDescription));
+                indicator.setTranslationDescription(translationDescription);
+
+                violations.addAll(indicatorValidator.validator(indicator));
+                var savedIndicator = indicatorRepository.save(indicator);
+
+                for (HowToCalculateDTO howToCalculateDTO : indicator_dto.getHowToCalculateList()) {
+                    var htc = new HowToCalculate();
+                    htc.setIndicator(savedIndicator);
+                    htc.setDescription(howToCalculateDTO.getDescription());
+                    violations.addAll(howToCalculateValidator.validator(htc));
+                    howToCalculateRepository.save(htc);
+                }
+            }
+        }
+
+        for (CalculationDTO calculation_dto : documentDTO.getCalculations()) {
+            var calculation = new Calculation();
+            calculation.setDocument(savedDoc);
+            calculation.setCommand(calculation_dto.getCommand());
+            calculation.setDescription(calculation_dto.getDescription());
+            violations.addAll(calculationValidator.validator(calculation));
+            calculationRepository.save(calculation);
+
+        }
         if (!violations.isEmpty()) {
             return violations;
         } else {
-            documentRepository.save(document);
             return Collections.emptySet();
         }
     }
