9403bcb70dfeeb79a2479fc92a80522b470fd267
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-10-07 23:51:44 +0330

    Removing unnecessary methods

--- src/main/java/de/miladsa/behpaya/model/Document.java
+++ src/main/java/de/miladsa/behpaya/model/Document.java
@@ -53,31 +53,31 @@ public class Document extends Metadata {
     )
     private List<Calculation> calculations = new ArrayList<>();
 
-    public void removeCalculation(Calculation calculation) {
-        if (this.calculations.contains(calculation)) {
-            this.calculations.remove(calculation);
-            calculation.setDocument(null);
-        }
-    }
-
-    public void addCalculations(Calculation calculation) {
-        if (!this.calculations.contains(calculation)) {
-            this.calculations.add(calculation);
-            calculation.setDocument(this);
-        }
-    }
-
-    public void removeBoard(Board board) {
-        if (this.boards.contains(board)) {
-            this.boards.remove(board);
-            board.setDocument(null);
-        }
-    }
-
-    public void addBoard(Board board) {
-        if (!this.boards.contains(board)) {
-            this.boards.add(board);
-            board.setDocument(this);
-        }
-    }
+//    public void removeCalculation(Calculation calculation) {
+//        if (this.calculations.contains(calculation)) {
+//            this.calculations.remove(calculation);
+//            calculation.setDocument(null);
+//        }
+//    }
+//
+//    public void addCalculations(Calculation calculation) {
+//        if (!this.calculations.contains(calculation)) {
+//            this.calculations.add(calculation);
+//            calculation.setDocument(this);
+//        }
+//    }
+//
+//    public void removeBoard(Board board) {
+//        if (this.boards.contains(board)) {
+//            this.boards.remove(board);
+//            board.setDocument(null);
+//        }
+//    }
+//
+//    public void addBoard(Board board) {
+//        if (!this.boards.contains(board)) {
+//            this.boards.add(board);
+//            board.setDocument(this);
+//        }
+//    }
 }
