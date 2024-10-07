b5a4ba55972900a20b626249b6db9699fcac142d
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-10-07 23:51:03 +0330

    Adding validation annotations for Translation.java

--- src/main/java/de/miladsa/behpaya/model/Translation.java
+++ src/main/java/de/miladsa/behpaya/model/Translation.java
@@ -1,6 +1,8 @@
 package de.miladsa.behpaya.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +17,13 @@ public class Translation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull(message = "Provide field fa")
+    @NotEmpty(message = "Field fa could not be empty")
     @Column(columnDefinition = "varchar(1000)", nullable = false)
     private String fa;
 
+    @NotNull(message = "Provide field en")
+    @NotEmpty(message = "Field en could not be empty")
     @Column(columnDefinition = "varchar(1000)", nullable = false)
     private String en;
 }
