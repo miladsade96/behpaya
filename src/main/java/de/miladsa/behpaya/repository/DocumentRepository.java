92ed5dd0de685d0bf5247677fa21d4f11baa7bc5
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-10-09 14:02:48 +0330

    Removing unnecessary codes

--- src/main/java/de/miladsa/behpaya/repository/DocumentRepository.java
+++ src/main/java/de/miladsa/behpaya/repository/DocumentRepository.java
@@ -1,15 +1,9 @@
 package de.miladsa.behpaya.repository;
 
 import de.miladsa.behpaya.model.Document;
-import de.miladsa.behpaya.projection.DocumentProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
-    @Query("select new de.miladsa.behpaya.projection.DocumentProjection(d.id,d.title,d.description,d.isHidden) from Document d")
-    List<DocumentProjection> findAllDocuments();
 }
